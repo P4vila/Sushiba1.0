855215260a6c68ae6839145e3725297ca92c70c1
jose907 <xemitaborder21@icloud.com>
2024-11-21 23:27:42 +0100

    FUENTE ROBOTO LIGHT

--- app/src/main/java/com/example/sushiba/LoginActivity.java
+++ app/src/main/java/com/example/sushiba/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.sushiba;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,20 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+
+
+
+        // Encontrar el TextView por su ID
+        TextView textView = findViewById(R.id.textView3);
+
+        // Establecer el texto del TextView usando el string desde resources
+        textView.setText(getString(R.string.title_login));
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
