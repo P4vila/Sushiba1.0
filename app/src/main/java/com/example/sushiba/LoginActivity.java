1590bbb7f6c86c80f31b96cfd9bf1cc0505da5e6
Lucipm25 <lucipm.49@gmail.com>
2024-11-23 23:52:48 +0100

    CAMBIO DE PAGO Y PERFIL

--- app/src/main/java/com/example/sushiba/LoginActivity.java
+++ app/src/main/java/com/example/sushiba/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.sushiba;
 
+import static com.example.sushiba.R.id.imageView6;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -45,6 +47,22 @@ public class LoginActivity extends AppCompatActivity {
                 .into(imageView2); // El ImageView donde se cargará la imagen
 
         // AQUI TERMINA LA CONFIGURACION DE GLIDE
+
+        // FOTO CON GLIDE DE PERFIL DE CAMILA
+        ImageView imageView6 = findViewById(R.id.imageView6);
+
+        // URL directa de la imagen que quieras usar
+        String imageUrl6 = "https://live.staticflickr.com/65535/54158695005_ab64ab8aa5_q.jpg";
+
+
+        // Usar Glide para cargar la imagen en el ImageView
+        Glide.with(this)
+                .load(imageUrl6)
+                .into(imageView6);
+
+
+
+
 
         // Encuentro el botón en el xml de LoginActivity
         Button btnEntrar = findViewById(R.id.btnEntrar);
