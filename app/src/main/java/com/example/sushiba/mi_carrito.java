e34802e50ba69aef2f80a5a361581b15e90424bf
Lucipm25 <lucipm.49@gmail.com>
2024-11-24 04:53:20 +0100

    JUAN ME HE QUEDADO HASTA LAS 5AM DESPIERTA

--- app/src/main/java/com/example/sushiba/mi_carrito.java
+++ app/src/main/java/com/example/sushiba/mi_carrito.java
@@ -1,7 +1,6 @@
 package com.example.sushiba;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
-
-public class soporte_ayuda extends AppCompatActivity {
+public class mi_carrito extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_soporte_ayuda);
+        setContentView(R.layout.activity_mi_carrito);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // IMAGEN DE LOGO SIN FONDO CON EL GLIDE IMPLEMENTADO- IMAGEN DE ICON EN SOPORTE Y AYUDA
-        ImageView imageView12 = findViewById(R.id.imageView12);
-        String imageUrl12 = "https://live.staticflickr.com/65535/54156304544_65b4df6fbe_q.jpg";
-        Glide.with(this)
-                .load(imageUrl12)
-                .into(imageView12);
     }
 }
